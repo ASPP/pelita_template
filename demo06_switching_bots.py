10c4ef6816250306397dfe9b8f0ccab3ee584576
Mathesis <jkahanovitz@tu-berlin.de>
2025-06-25 13:00:28 +0200

    adapt switching and background_games to new bot names

--- demo06_switching_bots.py
+++ demo06_switching_bots.py
@@ -1,52 +1,52 @@
-# This demo shows how to combine the strategies of the basic attacker and the
-# basic defender
-# Initially, one bot is in attack mode, while the other uses the defending
-# strategy. Once the attacking bot is killed, it restarts as a defender, while
-# the previous defender changes its personality and becomes the new attacker.
+# This demo shows how to combine the strategies of the basic gatherer and the
+# basic hunter
+# Initially, one bot is in gather mode, while the other uses the hunting
+# strategy. Once the gathering bot is killed, it restarts as a hunter, while
+# the previous hunter changes its personality and becomes the new gatherer.
 
-from demo05_basic_defender import move as move_defender
-from demo04_basic_attacker import move as move_attacker
+from demo05_basic_hunter import move as move_hunter
+from demo04_basic_gatherer import move as move_gatherer
 
 
 TEAM_NAME = 'Switching Bots'
 
 def init_state(personality):
     return {
-        # specifies which personality we are: "attacker" or "defender"
+        # specifies which personality we are: "gatherer" or "hunter"
         "personality": personality,
 
-        # entries prefixed with "attack_" are used by the move_attacker function
-        "attack_target": None,
-        "attack_path": None,
+        # entries prefixed with "gatherer_" are used by the move_gatherer function
+        "gatherer_target": None,
+        "gatherer_path": None,
 
-        # entries prefixed with "defend_" are used by the move_defender function
-        "defend_target": None,
-        "defend_path": None,
+        # entries prefixed with "hunter_" are used by the move_hunter function
+        "hunter_target": None,
+        "hunter_path": None,
     }
 
 def move(bot, state):
     # Our state consists of two “substates”, one for each bot.
     # In order for the substates to work properly with the imported
-    # `move_attacker` and `move_defender` funcions, we need to be sure
+    # `move_gatherer` and `move_hunter` funcions, we need to be sure
     # that the relevant attributes in the state are properly prefixed
     # (and each of the functions only works with “their” prefixed version).
 
     if state == {}:
         # here each bot has its own state dictionary (0 and 1)
-        state[0] = init_state("attacker")
-        state[1] = init_state("defender")
+        state[0] = init_state("gatherer")
+        state[1] = init_state("hunter")
 
-    # Only the attacker can go into the enemy zone and be killed. Therefore
-    # we only need to switch roles from the perspective of the defender.
+    # Only the gatherer can go into the enemy zone and be killed. Therefore
+    # we only need to switch roles from the perspective of the hunter.
     if bot.other.was_killed:
-        state[bot.turn]["personality"] = "attacker"
-        state[bot.other.turn]["personality"] = "defender"
+        state[bot.turn]["personality"] = "gatherer"
+        state[bot.other.turn]["personality"] = "hunter"
 
-    if state[bot.turn]["personality"] == "attacker":
-        next_pos = move_attacker(bot, state)
-        bot.say('attacker')
+    if state[bot.turn]["personality"] == "gatherer":
+        next_pos = move_gatherer(bot, state)
+        bot.say('gatherer')
     else:
-        next_pos = move_defender(bot, state)
-        bot.say('defender')
+        next_pos = move_hunter(bot, state)
+        bot.say('hunter')
     return next_pos
 
